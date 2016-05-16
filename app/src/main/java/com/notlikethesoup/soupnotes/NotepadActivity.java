3846c74d64d7fb8fc242f5e9bf5991fcf12fbc9c
Benjamin Campbell <benjamincampbell@GLEIS.local>
2016-05-16 14:14:17 -0400

    first commit

---
+++ app/src/main/java/com/notlikethesoup/soupnotes/NotepadActivity.java
@@ -0,0 +1,13 @@
+package com.notlikethesoup.soupnotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotepadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notepad);
+    }
+}
