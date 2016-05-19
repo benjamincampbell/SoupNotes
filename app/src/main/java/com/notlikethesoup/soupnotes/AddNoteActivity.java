d96f4873b6f1e6afe67cad0d2046c9a2d6d3e32f
benjamincampbell <twitch043@gmail.com>
2016-05-18 23:02:43 -0400

    trying to add notes, almost there

---
+++ app/src/main/java/com/notlikethesoup/soupnotes/AddNoteActivity.java
@@ -0,0 +1,39 @@
+package com.notlikethesoup.soupnotes;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.notlikethesoup.soupnotes.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.addnote, menu);
+
+        return true;
+    }
+
+    public void onConfirmNote(MenuItem mi){
+        Intent resultIntent = new Intent();
+        EditText editText = (EditText) findViewById(R.id.editNote);
+        String noteText = editText.getText().toString();
+        resultIntent.putExtra(EXTRA_MESSAGE, noteText);
+
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
+    }
+}
--- app/src/main/java/com/notlikethesoup/soupnotes/NotepadActivity.java
+++ app/src/main/java/com/notlikethesoup/soupnotes/NotepadActivity.java
@@ -1,18 +1,48 @@
 package com.notlikethesoup.soupnotes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NotepadActivity extends AppCompatActivity {
+
+    private ArrayList<String> notes;
+    private ArrayAdapter<String> notesAdapter;
+    private ListView lvNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notepad);
+        //   ^ actually displays the /res/layout/activity_notepad.xml
+
+        lvNotes = (ListView) findViewById(R.id.lvNotes);
+        notes = new ArrayList<>();
+        notesAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, notes);
+        lvNotes.setAdapter(notesAdapter);
+        notes.add("First");
+        notes.add("Second");
     }
 
-    public void newNote(View view){
+    public void onAddNote(View view){
+        Intent intent = new Intent(this, AddNoteActivity.class);
 
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null){
+            String newNote = data.getStringExtra("com.notlikethesoup.soupnotes.MESSAGE");
+            notes.add(newNote);
+            notesAdapter.notifyDataSetChanged();
+        }
     }
 }
